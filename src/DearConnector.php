ad88c56fea48a49b01168aa0db3d84165c8d2b0b
stephenjreid <sreid@gwlab.com.au>
2021-08-25 15:06:52 +1000

    Update with functions

--- src/DearConnector.php
+++ src/DearConnector.php
@@ -6,21 +6,58 @@ use Illuminate\Support\Facades\Http;
 
 class DearConnector
 {
-    public function CallDEAR($endpoint,$method,$body)
+    const CONTENT_TYPE = 'application/json';
+    const LIMIT = 100;
+    const PAGE = 1;
+
+    protected function getHeaders()
+    {
+        return [
+            'Content-Type' => self::CONTENT_TYPE,
+            'api-auth-accountid' => config('DearConnector.accountid'),
+            'api-auth-applicationkey' => config('DearConnector.applicationkey')
+        ];
+    }
+
+    public function CallDEAR($endpoint,$method,$parameters)
     {  
         $url = config('DearConnector.baseUrl').$endpoint;
         
         try
         {
-            $response = Http::withHeaders([
-                'api-auth-accountid' => config('DearConnector.accountid'),
-                'api-auth-applicationkey' => config('DearConnector.applicationkey')
-            ])->retry(3, 500)->acceptJson()->get($url, $body);
 
+        //add request to DB
+        //pagination?
+        //API call limit of 60 per minute
+
+
+            dd($this->getHeaders());
+
+            if ($method == 'POST' || $method == 'PUT') {
+                $requestParams['body'] = json_encode($parameters);
+            } else {
+                $requestParams['query'] = $parameters;
+            }
+
+            $response = Http::withHeaders($this->getHeaders())->retry(3, 500)->acceptJson()->get($url, $requestParams);
+
+            //add response to DB
+
+            //Handle errors
             return $response->getBody()->getContents();
 
-        } catch (Exception $ex) {
-            return($ex);
+        } catch (ClientException $clientException) {
+            return $clientException;
+        }
+        
+        catch (ServerException $serverException) {
+
+            if ($ex->getResponse()->getStatusCode() === 503) {
+                // API limit exceeded
+                sleep(5);
+                return $this->CallDEAR($endpoint,$method,$body);
+            }
+            return $serverException;
         }
     }
 }
